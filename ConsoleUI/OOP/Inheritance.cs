403e390dd8a529ec92e04f35c6efb3461055059a
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-11-14 11:17:23 +0300

    add inheritance example

---
+++ ConsoleUI/OOP/Inheritance.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Inheritance
+
+namespace ConsoleUI.OOP;
+
+// Concrete -> Abstract
+public abstract class Human // : Object
+{
+    protected abstract bool Telemetry { get; set; }
+
+    // public abstract void GetTelemetryData();
+    // abstract keyword bizim için soyutluğu ifade ediyor.
+    // Dolayısıyla program tarafında kullanabilmek somut karşılığı oluşturulması gerekiyor.
+
+    public virtual void GetTelemetryData()
+    {
+        if (Telemetry == false)
+            return;
+
+        Console.WriteLine("Sistemde 30 saniye durdu.");
+    }
+}
+
+public class User : Human // Tekli miras
+{
+    public string Email { get; set; }
+    protected string Password { get; private set; }
+    protected override bool Telemetry { get; set; } = true;
+
+    public void ForgetPassword()
+    {
+        Console.WriteLine(
+            "Şifre yenileme bağlantısı gönderildi."
+            );
+    }
+
+    public void SetPassword(string newPassword) // Encapsulation örneği
+    {
+        this.Password = HashPassword(newPassword);
+    }
+
+    public override void GetTelemetryData()
+    {
+        if (Telemetry == false)
+            return;
+
+        base.GetTelemetryData();
+        Console.WriteLine(
+            "Kullanıcı, sayfayı yeniledi."
+            );
+    }
+
+    private string HashPassword(string newPassword)
+    {
+        return newPassword + "Hash123XYZ";
+    }
+}
+
+public class Customer : User // is-a ilişkisi // Çok düzeyli miras
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    //public void CheckPasswordStrong()
+    //{
+    //    Console.WriteLine(Password);
+    //    SetPassword("");
+    //    //Password = 'yeniSifre';
+    //}
+}
+
+public class Seller : User
+{
+    public string Name { get; set; }
+}
+
+public class Product
+{
+
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleUI.OOP;
+
+// Inheritance
+
+User user = new User();
+user.Email = "Test@test.com";
+user.SetPassword("password");
+Console.WriteLine("User");
+Console.WriteLine(user.Email);
+
+user.GetTelemetryData();
+
+Customer customer = new Customer();
+customer.Email = "customer@test.com";
+customer.SetPassword("password");
+
+customer.FirstName = "Ahmet";
+customer.LastName = "Çetinkaya";
+
+Console.WriteLine("Customer");
+Console.WriteLine(customer.Email);
+Console.WriteLine(customer.FirstName);
+Console.WriteLine(customer.LastName);
+
+customer.ForgetPassword();
+
+Seller seller = new Seller();
+seller.Email = "bestseller@test.com";
+seller.SetPassword("password");
+seller.Name = "Best Seller";
+
+Console.WriteLine("Seller");
+Console.WriteLine(seller.Email);
+Console.WriteLine(seller.Name);
+
+seller.ForgetPassword();
