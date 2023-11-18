54435ceb86d0b742301e8c7eddf62e40a358a546
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-11-18 11:30:48 +0300

    add model entity

---
+++ Entities/Concrete/Model.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+
+namespace Entities.Concrete;
+
+public class Model : Entity<int>
+{
+    public int BrandId { get; set; } // foreign key
+    public string Name { get; set; }
+    public decimal DailyPrice { get; set; }
+}
